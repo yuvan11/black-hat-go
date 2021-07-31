cb5dc6a029cd3fd3401f7a9693f37aafbc38bdd1
yuvaraj_11 <yuva.ram11@gmail.com>
2021-08-01 00:47:01 +0530

    TCP fast scanner

---
+++ chapter2/tcp-fast-scanner/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func workers(ports chan int, wg *sync.WaitGroup) {
+	for p := range ports {
+		fmt.Println(p)
+		wg.Done()
+	}
+}
+
+func main() {
+	ports := make(chan int, 100)
+	var wg sync.WaitGroup
+
+	for i := 0; i < cap(ports); i++ {
+
+		go workers(ports, &wg)
+	}
+
+	for i := 1; i < 1024; i++ {
+		wg.Add(1)
+
+		ports <- i
+
+	}
+	wg.Wait()
+	close(ports)
+
+}
